64df39aa8194429b6ffd814633b0bb1e74a87f2f
Guy176 <guy.paes176@gmail.com>
2025-03-27 02:46:18 -0300

    feat(deposito): update ServiceGeral
    - Este commit atualiza a função de realizarConsulta4, visando retornar um objeto com as informações necessárias para criar o PDF requisitado.

--- agencia/src/main/java/agencia/agencia/service/ServiceGeral.java
+++ agencia/src/main/java/agencia/agencia/service/ServiceGeral.java
@@ -1,11 +1,13 @@
 package agencia.agencia.service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import agencia.agencia.repository.ClienteRepository;
+import agencia.agencia.repository.DepositoRepository;
 import agencia.agencia.service.model.Cliente;
 
 /*
@@ -17,6 +19,9 @@ public class ServiceGeral {
     @Autowired
     private ClienteRepository clienteRepository;
 
+    @Autowired
+    private DepositoRepository depositoRepository;
+
     public List<Cliente> realizarConsulta1() {
         return clienteRepository.consulta1();
     }
@@ -24,4 +29,25 @@ public class ServiceGeral {
     public List<String> realizarConsulta2() {
         return clienteRepository.consulta2();
     }
+
+    public List<Object[]> realizarConsulta4() {
+
+        // Define data limite (5 anos atrás)
+        LocalDate date = LocalDate.of(LocalDate.now().getYear(), 1, 1);
+        LocalDate dataLimite = date.minusYears(5);
+
+        // Realiza a consulta considerando a data limite
+        List<Object[]> resultadosBrutos = depositoRepository.consulta4(dataLimite);
+
+        List<Object[]> resultadosConvertidos = resultadosBrutos.stream()
+                .map(arr -> new Object[] {
+                        ((Number) arr[0]).intValue(), // Converte o ano para Integer
+                        ((Number) arr[1]).intValue(), // Converte o mês para Integer
+                        ((Number) arr[2]).intValue() // Converte o total de transações para Integer
+                })
+                .toList();
+
+        return resultadosConvertidos;
+    }
+
 }
